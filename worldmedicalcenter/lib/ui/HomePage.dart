6182b4705add5b9c569eb485e249a9b30a0d7685
Paulos Dessie Nedaw <pauldessie@gmail.com>
2023-01-20 15:32:23 +0300

    looks except document and prof

--- worldmedicalcenter/lib/ui/HomePage.dart
+++ worldmedicalcenter/lib/ui/HomePage.dart
@@ -1,3 +1,5 @@
+import 'package:expandable/expandable.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
@@ -8,12 +10,10 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  var isDropped = [false, false, false, false, false];
-  var showProfile = false;
-
   @override
   Widget build(BuildContext context) {
     List<String> titleList = [
+      "profile",
       "Allergies",
       "Medicine",
       "Diagnoses",
@@ -21,11 +21,42 @@ class _HomePageState extends State<HomePage> {
       "Documents",
     ];
     List<Icon> iconList = [
+      const Icon(Icons.no_accounts_rounded),
       const Icon(Icons.difference_sharp),
       const Icon(Icons.medication),
       const Icon(Icons.sd_card),
       const Icon(Icons.difference_sharp),
       const Icon(Icons.folder),
+    ];
+    List buttonList = [
+      [
+        "Translate",
+        Icon(
+          CupertinoIcons.globe,
+          size: 20,
+        )
+      ],
+      [
+        "Add",
+        Icon(
+          Icons.add,
+          size: 20,
+        )
+      ],
+      [
+        "Edit",
+        Icon(
+          Icons.edit,
+          size: 20,
+        )
+      ],
+      [
+        "Share",
+        Icon(
+          Icons.share,
+          size: 20,
+        )
+      ]
     ];
 
     return SafeArea(
@@ -35,7 +66,11 @@ class _HomePageState extends State<HomePage> {
           elevation: 0,
           title: Row(
             children: [
-              Icon(Icons.waving_hand, color: Colors.amber[400]),
+              Icon(
+                Icons.waving_hand,
+                color: Colors.amber[400],
+                size: 24,
+              ),
               const SizedBox(
                 width: 10,
               ),
@@ -44,7 +79,7 @@ class _HomePageState extends State<HomePage> {
                 style: const TextStyle(
                     color: Colors.black87,
                     fontWeight: FontWeight.bold,
-                    fontSize: 20),
+                    fontSize: 24),
               ),
             ],
           ),
@@ -53,107 +88,224 @@ class _HomePageState extends State<HomePage> {
               onPressed: () {},
               icon: const Icon(Icons.more_vert),
               color: Colors.black,
-              iconSize: 20,
+              iconSize: 24,
             )
           ],
         ),
         body: Container(
           color: Colors.blueGrey[50],
-          child: ListView(children: [
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Card(
-                elevation: 3,
-                child: Padding(
-                  padding: const EdgeInsets.symmetric(vertical: 4.0),
-                  child: ListTile(
-                    title: Text("Paulos Dessie"),
-                    subtitle: Text("Member since 2022"),
-                    leading: CircleAvatar(
-                      radius: 35,
-                      backgroundColor: Colors.grey[200],
-                      child: Icon(
-                        Icons.person,
-                        size: 40,
-                        color: Colors.grey[800],
-                      ),
-                    ),
-                  ),
-                ),
-              ),
-            ),
-            Container(
-              height: MediaQuery.of(context).size.height * 0.75,
-              child: ListView.builder(
-                itemCount: 5,
-                itemBuilder: (BuildContext context, int index) {
-                  return GestureDetector(
-                    onTap: () => {
-                      setState((() => isDropped[index] = !isDropped[index]))
-                    },
+          child: ListView.builder(
+            itemCount: 7,
+            itemBuilder: (BuildContext context, int index) {
+              if (index == 0) {
+                return Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Card(
+                    elevation: 3,
                     child: Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: Card(
-                        elevation: 3,
-                        child: Container(
-                          margin: const EdgeInsets.symmetric(vertical: 2),
-                          decoration: BoxDecoration(
-                              color: Colors.white,
-                              borderRadius: BorderRadius.circular(20)),
-                          child: Padding(
-                            padding: const EdgeInsets.symmetric(
-                                horizontal: 12, vertical: 10),
-                            child: Row(
-                                mainAxisAlignment:
-                                    MainAxisAlignment.spaceBetween,
-                                children: [
-                                  Row(
-                                    children: [
-                                      iconList[index],
-                                      const SizedBox(
-                                        width: 8,
-                                      ),
-                                      Column(
-                                        crossAxisAlignment:
-                                            CrossAxisAlignment.start,
-                                        children: [
-                                          Text(titleList[index],
-                                              style: const TextStyle(
-                                                  fontSize: 16)),
-                                          Text(
-                                            "No allergies Listed",
-                                            style: const TextStyle(
-                                                fontSize: 10,
-                                                color: Colors.black54),
-                                          )
-                                        ],
-                                      )
-                                    ],
-                                  ),
-                                  checkDropped(index),
-                                ]),
+                      padding: const EdgeInsets.symmetric(vertical: 4.0),
+                      child: ListTile(
+                        title: Text("Paulos Dessie"),
+                        subtitle: Text("Member since 2022"),
+                        leading: CircleAvatar(
+                          radius: 35,
+                          backgroundColor: Colors.grey[200],
+                          child: Icon(
+                            Icons.person,
+                            size: 40,
+                            color: Colors.grey[800],
                           ),
                         ),
                       ),
                     ),
-                  );
-                },
-              ),
-            ),
-          ]),
+                  ),
+                );
+              } else if (index == 6) {
+                return Padding(
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 15, vertical: 10),
+                  child: Container(
+                    clipBehavior: Clip.antiAlias,
+                    height: 190,
+                    decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(5)),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Image.network(
+                          "https://img.freepik.com/free-vector/blue-dna-blue-medical-healthcare-background_1017-24093.jpg?w=996&t=st=1674216835~exp=1674217435~hmac=72cc1dd7e369e0bfc4b3162ecae072d8d3ce99bddd16469c66521c89014f6421",
+                          width: double.maxFinite,
+                          height: 150,
+                          fit: BoxFit.cover,
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.only(
+                              left: 8.0, top: 13),
+                          child: Text("Card will Expire in 208 days"),
+                        ),
+                      ],
+                    ),
+                  ),
+                );
+              }
+              return ExpandableNotifier(
+                child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Card(
+                      child: Padding(
+                        padding: const EdgeInsets.all(0.0),
+                        child: ScrollOnExpand(
+                          child: ExpandablePanel(
+                              header: Padding(
+                                padding: const EdgeInsets.only(
+                                    left: 15, top: 15, bottom: 15, right: 15),
+                                child: Row(
+                                  crossAxisAlignment: CrossAxisAlignment.center,
+                                  mainAxisAlignment: MainAxisAlignment.start,
+                                  children: [
+                                    iconList[index],
+                                    const SizedBox(
+                                      width: 12,
+                                    ),
+                                    Column(
+                                      crossAxisAlignment:
+                                          CrossAxisAlignment.start,
+                                      children: [
+                                        Text(titleList[index],
+                                            style:
+                                                const TextStyle(fontSize: 18)),
+                                        SizedBox(
+                                          height: 2,
+                                        ),
+                                        Text(
+                                          "No allergies Listed",
+                                          style: const TextStyle(
+                                              fontSize: 14,
+                                              color: Colors.black54),
+                                        )
+                                      ],
+                                    )
+                                  ],
+                                ),
+                              ),
+                              collapsed: SizedBox.shrink(),
+                              expanded: Padding(
+                                padding:
+                                    const EdgeInsets.only(left: 15, right: 15),
+                                child: Column(
+                                  children: [
+                                    Container(
+                                      margin: EdgeInsets.only(
+                                          bottom: 8, left: 3, right: 3),
+                                      height: 1,
+                                      color: Colors.blueGrey[200],
+                                    ),
+                                    Container(
+                                      height:
+                                          MediaQuery.of(context).size.height *
+                                              0.5,
+                                      padding: EdgeInsets.symmetric(
+                                          vertical: 2, horizontal: 10),
+                                      child: ListView.builder(
+                                          itemCount: 15,
+                                          itemBuilder: (context, index) {
+                                            return Padding(
+                                              padding:
+                                                  const EdgeInsets.symmetric(
+                                                      vertical: 6.0),
+                                              child: Column(
+                                                crossAxisAlignment:
+                                                    CrossAxisAlignment.start,
+                                                children: [
+                                                  Text(
+                                                    "Saccharomyces ellipsoideus dkhasdasb dsakdsahbd khasgdhsa hdsagdhasgdh",
+                                                    style: TextStyle(
+                                                        fontWeight:
+                                                            FontWeight.w400,
+                                                        fontSize: 16),
+                                                  ),
+                                                  SizedBox(
+                                                    height: 6,
+                                                  ),
+                                                  Text(
+                                                    "WMCX0002",
+                                                    style: TextStyle(
+                                                        color: Colors.black45,
+                                                        // fontWeight:
+                                                        //     FontWeight.w200,
+                                                        fontSize: 12),
+                                                  )
+                                                ],
+                                              ),
+                                            );
+                                          }),
+                                    ),
+                                    Container(
+                                      height: 70,
+                                      padding:
+                                          EdgeInsets.symmetric(vertical: 15),
+                                      child: ListView.builder(
+                                          itemCount: 4,
+                                          scrollDirection: Axis.horizontal,
+                                          itemBuilder: ((context, index) {
+                                            return Container(
+                                              margin: EdgeInsets.symmetric(
+                                                  horizontal: 5),
+                                              child: InkWell(
+                                                onTap: (() {}),
+                                                borderRadius:
+                                                    BorderRadius.circular(100),
+                                                child: Container(
+                                                  padding: EdgeInsets.symmetric(
+                                                      horizontal: 20,
+                                                      vertical: 4),
+                                                  decoration: BoxDecoration(
+                                                      borderRadius:
+                                                          BorderRadius.circular(
+                                                              100),
+                                                      border: Border.all(
+                                                          width: 0.5,
+                                                          color: Colors.black45,
+                                                          style: BorderStyle
+                                                              .solid)),
+                                                  child: Row(
+                                                    mainAxisAlignment:
+                                                        MainAxisAlignment
+                                                            .center,
+                                                    children: [
+                                                      buttonList[index][1],
+                                                      SizedBox(
+                                                        width: 6,
+                                                      ),
+                                                      Text(
+                                                        buttonList[index][0],
+                                                        style: TextStyle(
+                                                            fontSize: 14,
+                                                            color:
+                                                                Colors.black),
+                                                      )
+                                                    ],
+                                                  ),
+                                                ),
+                                              ),
+                                            );
+                                          })),
+                                    ),
+                                    SizedBox(
+                                      height: 6,
+                                    )
+                                  ],
+                                ),
+                              )),
+                        ),
+                      ),
+                    )),
+              );
+            },
+          ),
         ),
       ),
     );
-  }
-
-  Widget checkDropped(i) {
-    return isDropped[i]
-        ? const Icon(Icons.arrow_drop_up)
-        : const Icon(Icons.arrow_drop_down);
-  }
-  Widget checkShowProfile() {
-    return showProfile
-        ? const Icon(Icons.arrow_drop_up)
-        : const Icon(Icons.arrow_drop_down);
   }
 }
