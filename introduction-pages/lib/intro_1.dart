49140b311ce3e23f65da68f067dc2f9773aca723
kalish93 <kaleabtekalign@gmail.com>
2023-02-07 10:06:30 +0300

    adding introduction pages

---
+++ introduction-pages/lib/intro_1.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+
+
+class IntroductionOne extends StatefulWidget {
+  IntroductionOne({Key? key}) : super(key: key);
+
+  @override
+  State<IntroductionOne> createState() => _IntroductionOneState();
+}
+
+class _IntroductionOneState extends State<IntroductionOne> {
+  @override
+  Widget build(BuildContext context) {
+    return  Padding(
+          padding: const EdgeInsets.all(10),
+          child: Column(children: [
+            SizedBox(height: 90,),
+            ClipRRect(
+              borderRadius: BorderRadius.circular(10),
+              child: Image(
+              image: AssetImage('assets/intro1.webp'),
+              height: 150,
+              ),
+              
+            ),
+              SizedBox(height: 50,),
+              Text(
+                "Fill Your Info", 
+                style: TextStyle(fontSize: 25,
+                fontWeight: FontWeight.bold),
+                ),
+                Padding(padding: EdgeInsets.all(20),
+                child: Text('Your medical information is coded according to World Health Organizations global standards (ICD-10 ATC) to ensure that your condition and requirement is understood by health profesionals.',
+                textAlign: TextAlign.center,
+                style: TextStyle(color: Colors.black54 ),
+                ),
+                ),
+          
+          ],
+          
+          )
+          );
+  }
+}
---
+++ introduction-pages/lib/login.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+
+
+
+class Login extends StatefulWidget {
+  Login({Key? key}) : super(key: key);
+
+  @override
+  State<Login> createState() => _LoginState();
+}
+
+class _LoginState extends State<Login> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          Text('Login to world Medical Card', style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),),
+          Text('How would you like to sign-in?', style: TextStyle(fontSize: 11, color: Colors.black54),),
+          TextButton(onPressed: (){}, child: Text('Sign-in with google')),
+          
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+   Row(children: [
+            Expanded(
+              child: Container(
+                  margin:const EdgeInsets.only(left: 10.0, right: 15.0),
+                  child: Divider(
+                    color: Colors.black,
+                    height: 50,
+                  )),
+            ),
+ 
+            Text("OR"),
+ 
+            Expanded(
+              child: new Container(
+                  margin: const EdgeInsets.only(left: 15.0, right: 10.0),
+                  child: Divider(
+                    color: Colors.black,
+                    height: 50,
+                  )),
+            ),
+          ]),
+
+
+
+        ],
+      ),
+    );
+  }
+}
