01eaad4344b2b2374679b3ca8bf1438a6c1a6f15
kalish93 <kaleabtekalign@gmail.com>
2023-02-11 16:39:45 +0300

    adding theme

--- worldmedicalcenter/lib/main.dart
+++ worldmedicalcenter/lib/main.dart
@@ -19,15 +19,39 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      routes: {"/add": (context) => Add()},
+      title: 'World Medical App',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        // This is the theme of your application.
+        //
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or simply save your changes to "hot reload" in a Flutter IDE).
+        // Notice that the counter didn't reset back to zero; the application
+        // is not restarted.
+        // backgroundColor: Color.fromARGB(255, 180, 231, 255),
+        primaryTextTheme: const TextTheme(
+            bodyText1: TextStyle(fontFamily: "Nunito Sans", fontSize: 10)),
+        inputDecorationTheme: const InputDecorationTheme(
+          enabledBorder: OutlineInputBorder(
+            // borderSide: BorderSide(width: 1, color: Colors.black),
+            borderRadius: BorderRadius.all(Radius.circular(15)),
+          ),
+          focusedBorder: OutlineInputBorder(
+            borderSide: BorderSide(width: 1, color: Colors.blue),
+            borderRadius: BorderRadius.all(Radius.circular(10)),
+          ),
+        ),
       ),
-      home: Splash(),
       debugShowCheckedModeBanner: false,
+      home: Splash(),
     );
   }
+
 }
+
 
 
 class MyHomePage extends StatefulWidget {
