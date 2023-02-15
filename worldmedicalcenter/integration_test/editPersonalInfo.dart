66c5dfd2f04ad7989c461cce4f830d6c8391fab9
Nahom32 <senaynahom00@gmail.com>
2023-02-15 11:39:33 +0300

    Changes made to the system

--- worldmedicalcenter/integration_test/editPersonalInfo.dart
+++ worldmedicalcenter/integration_test/editPersonalInfo.dart
@@ -1,7 +1,8 @@
+// ignore_for_file: import_of_legacy_library_into_null_safe
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:integration_test/integration_test.dart';
-
 import 'package:worldmedicalcenter/main.dart' as app;
 
 void main() {
@@ -12,7 +13,7 @@ void main() {
     await tester.pumpAndSettle();
     await tester.pumpAndSettle();
 
-    await Future.delayed(Duration(milliseconds: 2000), () {});
+    await Future.delayed(const Duration(milliseconds: 2000), () {});
     final Finder nextText = find.byKey(const Key("skip"));
     // final Finder text = find.text("Skip Tour");
     // print(nextText);
@@ -23,18 +24,18 @@ void main() {
     await tester.pumpAndSettle();
     await tester.tap(nextText);
     await tester.pumpAndSettle();
-    await Future.delayed(Duration(milliseconds: 1000), () {});
+    await Future.delayed(const Duration(milliseconds: 1000), () {});
     // print("=======================================");
     await tester.pumpAndSettle();
     final Finder logingbutton = find.byKey(const Key("Login"));
 
     await tester.tap(logingbutton);
     await tester.pumpAndSettle();
-    await Future.delayed(Duration(milliseconds: 1000), () {});
+    await Future.delayed(const Duration(milliseconds: 1000), () {});
     await tester.pumpAndSettle();
     final Finder loginbtn = find.byKey(const Key("Login"));
     final Finder EmailForm = find.byKey(const Key("Email"));
-    final Finder passwordForm = find.byKey(Key("Password"));
+    final Finder passwordForm = find.byKey(const Key("Password"));
 
     await tester.pumpAndSettle();
 
@@ -42,24 +43,24 @@ void main() {
     await tester.enterText(passwordForm, "password");
 
     await tester.pumpAndSettle();
-    await tester.tap(logingbutton);
-    await Future.delayed(Duration(milliseconds: 3000), () {});
+    await tester.tap(loginbtn);
+    await Future.delayed(const Duration(milliseconds: 3000), () {});
 
-    final Finder expandPersonal = find.byKey(Key("PersonalInfo"));
-    final Finder mainContainer = find.byKey(Key("ScrollMain"));
-    final Finder editButton = find.byKey(Key("EditPersonal"));
+    final Finder expandPersonal = find.byKey(const Key("PersonalInfo"));
+    final Finder mainContainer = find.byKey(const Key("ScrollMain"));
+    final Finder editButton = find.byKey(const Key("EditPersonal"));
     await tester.tap(expandPersonal);
     await tester.pumpAndSettle();
 
-    await Future.delayed(Duration(milliseconds: 2000), () {});
+    await Future.delayed(const Duration(milliseconds: 2000), () {});
 
     await tester.drag(
         mainContainer,const Offset(0, -300));
-    await Future.delayed(Duration(milliseconds: 1000), () {}); 
+    await Future.delayed(const Duration(milliseconds: 1000), () {}); 
 
     await tester.pumpAndSettle();
     await tester.tap(editButton);
     await tester.pumpAndSettle();
-    await Future.delayed(Duration(milliseconds: 2000), () {});
+    await Future.delayed(const Duration(milliseconds: 2000), () {});
   });
 }
---
+++ worldmedicalcenter/test/alergy.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  // Define a test. The TestWidgets function also provides a WidgetTester
+  // to work with. The WidgetTester allows you to build and interact
+  // with widgets in the test environment.
+  testWidgets('MyWidget has a title and message', (WidgetTester tester) async {
+    await tester.pumpWidget(Scaffold(
+        body: Stack(
+      children: [],
+    )));
+
+    // Test code goes here.
+  });
+}
